9e20eae9971ec426d138c89dfb4dd3b8c46c9ef4
Suryansh Singh <suryanshx25@gmail.com>
2024-10-27 12:47:23 +0530

    re-commits

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-54-196-109-131.compute-1.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-3-108-217-173.ap-south-1.compute.amazonaws.com:5000/")
 
 
 # logging configuration
