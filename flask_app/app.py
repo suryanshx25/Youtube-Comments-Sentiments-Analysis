323574e8118eca8170250dedf98041894385a7ff
Suryansh Singh <suryanshx25@gmail.com>
2024-11-06 20:35:39 +0530

    5

--- flask_app/app.py
+++ flask_app/app.py
@@ -51,7 +51,7 @@ def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
     return model, vectorizer
 
 # Initialize the model and vectorizer
-model, vectorizer = load_model_and_vectorizer("yt_chrome_plugin_model", "2", "./tfidf_vectorizer.pkl")
+model, vectorizer = load_model_and_vectorizer("Youtube-Comments-Sentiment-Analyzer", "1", "./tfidf_vectorizer.pkl")
 
 @app.route('/predict', methods=['POST'])
 def predict():
