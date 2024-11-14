1cf13bc28fdff0777b93a00b4f47e06f95d58141
Suryansh Singh <suryanshx25@gmail.com>
2024-11-14 20:20:27 +0530

    fixed flask api testing

--- scripts/test_flask_api.py
+++ scripts/test_flask_api.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 import json
 
-BASE_URL = "http://localhost:5000"  # Replace with your deployed URL if needed
+BASE_URL = "http://localhost:5000"  # Replace with your deployed URL if needed.
 
 def test_predict_endpoint():
     data = {
