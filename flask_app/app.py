5d36e9e792e399871f931c53b193da33d3099b2b
Suryansh Singh <suryanshx25@gmail.com>
2025-01-24 13:21:10 +0530

    debug

--- flask_app/app.py
+++ flask_app/app.py
@@ -59,7 +59,7 @@ def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
     return model, vectorizer
 
 # Initialize the model and vectorizer
-model, vectorizer = load_model_and_vectorizer("Youtube-Comments-Sentiment-Analyzer", "7", "tfidf_vectorizer.pkl")  # Update paths and versions as needed
+model, vectorizer = load_model_and_vectorizer("Youtube-Comments-Sentiment-Analyzer", "2", "tfidf_vectorizer.pkl")  # Update paths and versions as needed
 
 @app.route('/')
 def home():
